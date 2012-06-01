76ea2cb3a55ea8fad142de7c4f3f84ad83b1a933
Robert (Jamie) Munro <rjmunro@arjam.net>
2012-06-02 00:06:17 +0100

    Initial commit of empty project.

---
+++ src/net/arjam/naughtystep/NaughtyStepActivity.java
@@ -0,0 +1,13 @@
+package net.arjam.naughtystep;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NaughtyStepActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
